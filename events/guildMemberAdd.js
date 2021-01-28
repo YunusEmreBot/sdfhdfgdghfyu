1779b9f6ca25b1d14f6e45dc5e57bfe3a9c9eb07
YunusEmreBot <60326972+YunusEmreBot@users.noreply.github.com>
2021-01-28 20:03:22 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+new Discord.User("clientdata");
+const channel = client.channels.get("803576080770400257");
+module.exports = member => {
+  
+  
+  channel.send(``);
+};
